7ffd0914d2209de83498a54dbb4beb2f28fd32c3
Miss-Siba <sibaquma@gmail.com>
2024-03-22 12:32:33 -0700

    packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,13 @@
 #!/usr/bin/puppet
 
-# Update Flask and Werkzeug to their latest versions
-exec { 'update_flask_werkzeug':
-  command => 'pip3 install --upgrade flask werkzeug',
-  path    => ['/usr/bin', '/usr/local/bin'],
+# Install Werkzeug to their latest versions
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
 # Install Flask version 2.1.0 using pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['update_flask_werkzeug'],
 }
