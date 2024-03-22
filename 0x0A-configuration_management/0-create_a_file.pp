6fe00b994eb536bfebfedc057f9f923ce3fb579d
Miss-Siba <sibaquma@gmail.com>
2024-03-22 11:16:44 -0700

    corrections

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# create_tmp_file.pp
+
 file { '/tmp/school':
   ensure  => present,
-  content => "I love Puppet",
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744'
