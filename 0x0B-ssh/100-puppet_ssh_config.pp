2e55e3b17c648c95037467dadb26f6e2fe7a39a1
Miss-Siba <sibaquma@gmail.com>
2024-03-24 10:14:37 -0700

    servers

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,13 +7,13 @@ file { '/etc/ssh/ssh_config':
 
 # Ensure SSH private key is configured
 file_line { 'SSH Private Key':
-  path               => '/etc/ssh/ssh_config',
-  line               => 'PasswordAuthentication no',
-  match              => '^#PasswordAuthentication',
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#PasswordAuthentication',
 }
 # Deny Password Authentication
 file_line { 'Deny Password ':
-  path               => '/etc/ssh/ssh_config',
-  line               => 'IdentityFile `/.ssh/school',
-  match              => '^#IdentityFile',
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile `/.ssh/school',
+  match => '^#IdentityFile',
 }
